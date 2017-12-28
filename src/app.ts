015e291b56a6d38a6e3f2d8908075705f89a9e87
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2017-12-28 19:41:09 +0100

    Lection https://platform.ultimateangular.com/courses/ngrx-store-effects/lectures/3788550 finished

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
+import * as fromStore from './store';
+
 import { renderTodos } from './utils';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
+
+const reducers = {
+  todos: fromStore.reducer,
+};
+
+const store = new fromStore.Store(reducers);
 
 button.addEventListener(
   'click',
@@ -12,7 +20,12 @@ button.addEventListener(
 
     const payload = { label: input.value, complete: false };
 
-    console.log(payload);
+    store.dispatch({
+      type: 'ADD_TODO',
+      payload,
+    });
+
+    console.log(store.value);
 
     input.value = '';
   },
